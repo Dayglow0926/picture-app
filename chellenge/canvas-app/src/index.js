f3e34b3cb810020e94b5115cd1a7b66fd42a6a45
Dayglow0926 <dayglow0926@gmail.com>
2022-10-28 01:35:46 +0900

    feat: 그림판 기능 과제

--- chellenge/canvas-app/src/index.js
+++ chellenge/canvas-app/src/index.js
@@ -9,3 +9,40 @@ canvas.height = 600;
 ctx.lineWidth = 5;
 let isPainting = false;
 let isFilling = false;
+
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting(){
+    isPainting = true;
+}
+function endPainting(){
+    isPainting = false;
+    if(isFilling){
+        ctx.fill();
+    }
+    ctx.beginPath();
+}
+function changeColor(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+function fillMode(){
+    isFilling = true;
+}
+function strokeMode(){
+    isFilling = false;
+}
+
+canvas.addEventListener("mousemove",onMove);
+canvas.addEventListener("mousedown",startPainting);
+canvas.addEventListener("mouseup",endPainting);
+canvas.addEventListener("mouseleave",endPainting);
+color.addEventListener("change",changeColor);
+fill.addEventListener("click", fillMode);
+stroke.addEventListener("click", strokeMode);
